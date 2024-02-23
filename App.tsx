8b3dd0180854ebff67b4a0ecccdc8e2233d3769d
BernardoliveiraFiap <RM551356@FIAP.COM.BR>
2024-02-23 09:14:45 -0300

    Fonte

--- App.tsx
+++ App.tsx
@@ -1,8 +1,28 @@
 import React from "react";
-import { Text } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+
 
 function App() {
-    return <Text>Mobile Enzo</Text>
+    return(
+      <View style = {style.container}>
+        <Text style = {style.txtG}>Mobile Enzo</Text>
+      </View>
+    ) 
 }
+
+const style = StyleSheet.create({
+  container:{
+    flexGrow:1,
+    backgroundColor:"#A00",
+    justifyContent:"center",
+    alignItems:"center"
+  },
+
+txtG:{
+  fontSize:30
+}
+})
+
+
 
 export default App;
